78e7850b871a234cb795a771a25204e8b80ba19c
David Chan <72647287+Luckder@users.noreply.github.com>
2025-08-22 20:26:18 +0800

    Create Path_1_Runology.js
    Added path 1

---
+++ Paths/Path_1_Runology.js
@@ -0,0 +1,42 @@
+//1) No
+
+//2) No
+
+//3) Both the blue heart and red heart runes are viewable and either can be selected to be displayed.
+
+//---------------------------------------------------------------------------------------------
+
+// The following are PREDECLARED:
+// red, blue, stack, heart, nova, show
+// Please do not import them.
+
+function love(rune) {
+    // edit the return expression
+    return stack(red(heart), rune);
+}
+
+show(love(blue(nova)));
+
+// To view the rune, click on the blinking icon on the right panel to see the image.
+// Don't forget to click on the airplane icon to run the tests :)
+// As a reminder, if the tests are open, then the run button will run all tests.
+
+//---------------------------------------------------------------------------------------------
+
+// The following are PREDECLARED:
+// show, stackn, quarter_turn_left, quarter_turn_right,
+// and all the basic runes: nova, heart, etc.
+// Please do not import them.
+// Do not import the beside, besiden, beside_frac functions.
+// They will not work.
+
+function besiden(n, rune) {
+    // edit the return expression
+    return quarter_turn_right(stackn(n, quarter_turn_left(rune)));
+}
+
+show(besiden(5, heart));
+
+// To view the rune, click on the blinking icon on the right panel to see the image.
+// Don't forget to click on the airplane icon to run the tests :)
+// As a reminder, if the tests are open, then the run button will run all tests.
