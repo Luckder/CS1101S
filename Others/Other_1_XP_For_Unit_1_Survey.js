d06811df10b701230de6d2b6f0e921dea65125c9
David Chan <72647287+Luckder@users.noreply.github.com>
2025-09-14 01:38:04 +0800

    Create Other_1_XP_For_Unit_1_Survey.js
    Added Other 1

---
+++ Others/Other_1_XP_For_Unit_1_Survey.js
@@ -0,0 +1,20 @@
+/*
+// example:
+const phone_number = 17783302389;
+*/
+
+const phone_number = 18005211980; // enter the number before the semicolon
+
+const did_the_survey = "Y" + "e" + "s";
+
+import {heart, show, red} from 'rune';
+
+if (did_the_survey === "Yes") {
+    const repeat = f => x => f(x);
+
+    repeat(display)("Dang, I didn't know you liked Star Wars. The phone number: '" + stringify(phone_number) + "', which was the 1979 Star Wars Hotline, allowed you to listen to various Star Wars characters, based on what I could gather via OSINT. I am guessing your favourite character is Darth Vader, just like me; if not, consider yourself an OP! https://www.youtube.com/watch?v=qHlH10fy0hA");
+
+    show(red(heart));
+} else {
+    display("Why not?");
+}
