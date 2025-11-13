f56639d5bd6d0bcb5662d8dc1a74ad22d7783e2a
David Chan <72647287+Luckder@users.noreply.github.com>
2025-11-13 20:33:06 +0800

    Create Actual.js

---
+++ Practical_Assessments/Actual.js
@@ -0,0 +1,25 @@
+function kolakoski() {
+
+    let stack = [1, 2, 2];
+
+    const helper = (odd, n) => {
+
+        const iter = stack[n];
+
+        for (let i = 0; i < iter; i = i + 1) {
+
+            if (odd) {
+                stack[array_length(stack)] = 1;
+            } else {
+                stack[array_length(stack)] = 2;
+            }
+
+        }
+
+        return pair(iter, () => helper(!odd, n + 1));
+
+    };
+
+    return pair(1, () => pair(2, () => helper(true, 2)));
+
+}
